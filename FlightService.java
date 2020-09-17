40d82a19554dd5ac90e8d993b9eb058f7c023e2a
Brian Levin <bml201095@gmail.com>
2020-09-17 02:11:42 -0400

    created a variable and methods which will have a list and then create a new list of the websites

--- FlightService.java
+++ FlightService.java
@@ -1,6 +1,8 @@
 public class FlightService { // pobluc class to hold the flgiht service object variables and methods
     public Stream<CompletableFuture<Quote>> getQuotes() { // created an async function which will take the stream api of the created array
+        var sites = List.of("site1", "site2", "site3"); // List allows postional access of the array
 
+        return sites.stream().map(this::getQuote); // take the stream of the array and return a new quote
 
     }
 
