868ba41dea2b01facfcbb81aac3e522962b27a15
Brian Levin <bml201095@gmail.com>
2020-09-17 02:57:08 -0400

    created variable and methods which will hold the flight service quote values and then print out the quotes as array methods

--- CompletableFuturesDemo.java
+++ CompletableFuturesDemo.java
@@ -1,6 +1,11 @@
-public class CompletableFuturesDemo {
-    public static void show() {
+public class CompletableFuturesDemo { // public class object which will hold the time methods
+    public static void show() { // main method which will show the time
 
+        var service = new FlightService(); // vairable to hold flight service object
+        
+        var futures = service.getQuotes()// variable which will get the quotes from the flgiht service method
+        .map(future -> future.thenAccept(System.out::println)) // create new array and then pass the values  to print out the new quotes
+        .collect(Collectors.toList()); // collects the methods and puts them in a list
 
     }
     
