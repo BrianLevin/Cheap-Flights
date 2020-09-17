67afb4f14f5d381b967125be2bba95a6697a23bd
Brian Levin <bml201095@gmail.com>
2020-09-17 01:56:37 -0400

    made variables whcih will hold the final site and price

--- Quote.java
+++ Quote.java
@@ -1,6 +1,7 @@
 public class Quote {
+    private final String site; // only this class has access to producing the site and wony change value
+    private final int price; // only this class has access to producing the site and wont chage value
 
 
 
-    
 }
