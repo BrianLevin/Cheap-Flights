afc5048906bfe02dd58bcffd2ce13ff1bab1c1c0
Brian Levin <bml201095@gmail.com>
2020-09-17 02:05:24 -0400

    created a async stream method which will get the site quotes

--- FlightService.java
+++ FlightService.java
@@ -1,4 +1,7 @@
 public class FlightService { // pobluc class to hold the flgiht service object variables and methods
-    
-    
+    public Stream<CompletableFuture<Quote>> getQuotes() { // created an async function which will take the stream api of the created array
+
+
+    }
+
 }
