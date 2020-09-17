a8e3009c0adb4c14c7f9a20156496eaa70ce505a
Brian Levin <bml201095@gmail.com>
2020-09-17 01:58:09 -0400

    made a constructorfor the site and price

--- Quote.java
+++ Quote.java
@@ -2,6 +2,9 @@ public class Quote {
     private final String site; // only this class has access to producing the site and wony change value
     private final int price; // only this class has access to producing the site and wont chage value
 
-
+    public Quote(String site, int price) { // made a constructor so I can utilze the site and price as methods.
+        this.site = site;
+        this.price = price;
+      }
 
 }
