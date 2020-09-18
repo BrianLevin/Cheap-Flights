6379d7f31cbdbe119420a2557395913636e04f0f
Brian Levin <bml201095@gmail.com>
2020-09-18 02:03:43 -0400

    made method which will calculate and print  the dirtion it took to get the quotes

--- CompletableFuturesDemo.java
+++ CompletableFuturesDemo.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 
 public class CompletableFuturesDemo { // public class object which will hold the time methods
     public static void show() { // main method which will show the time
-
+      var start = LocalTime.now(); // method to show the current time
         var service = new FlightService(); // vairable to hold flight service object
         
         var futures = service.getQuotes()// variable which will get the  futrue quotes from the flgiht service method
@@ -13,12 +13,12 @@ public class CompletableFuturesDemo { // public class object which will hold the
         .collect(Collectors.toList()); // collects the methods and puts them in a list
 
     
-        CompletableFuture
-        .allOf(futures.toArray(new CompletableFuture[0]))
-        .thenRun(() -> {
-          var end = LocalTime.now();
-          var duration = Duration.between(start, end);
-          System.out.println("Retrieved all quotes in " + duration.toMillis() + " msec.");
+        CompletableFuture //object which will hold the new array
+        .allOf(futures.toArray(new CompletableFuture[0])) // method to get the new array
+        .thenRun(() -> { // method to print code
+          var end = LocalTime.now(); // returns the current time
+          var duration = Duration.between(start, end); // get a dirtion object which will show the start time to the end time
+          System.out.println("Retrieved all quotes in " + duration.toMillis() + " msec."); // print out how ling it took inmilli seconds
         });
 
 
